e0b21f663488b3bec29f1a5f5eb51128a68ce102
parth5012 <parthchawla5012@GMAIL.COM>
2026-01-25 17:44:58 +0530

    Fixed Node Packages and django missing packages

--- backend/store/models.py
+++ backend/store/models.py
@@ -3,8 +3,8 @@ from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.db import models
 from uuid import uuid4
-
 from store.validators import validate_file_size
+
 
 
 class Promotion(models.Model):
